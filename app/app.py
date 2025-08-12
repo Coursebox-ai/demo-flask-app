18ec9c81d00f05ab638030df8c34ad281fd708b1
joydeep roychowdhury <joydeepbeyondsky86@gmail.com>
2025-08-12 16:30:04 +0530

    Fix app.py

--- app/app.py
+++ app/app.py
@@ -2,11 +2,9 @@ from flask import Flask
 from asgi_lifespan import LifespanManager
 from asgiref.wsgi import WsgiToAsgi
 
-flask_app = Flask(__name__)
 
 @flask_app.route("/")
 def hello():
     return "Hello from Flask + Uvicorn!"
 
-# ASGI adapter
-app = WsgiToAsgi(flask_app)
+app = Flask(__name__)
